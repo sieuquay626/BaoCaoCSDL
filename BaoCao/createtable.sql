dbb8081828e0ade673e2157da6e19a8100a9d59f
phatnguyen261098 <phatnguyen.pix@gmail.com>
2020-07-09 06:58:52 +0700

    first commit

---
+++ BaoCao/createtable.sql
@@ -0,0 +1,92 @@
+﻿create database Tour 
+use Tour
+
+create table CHUCVU(
+	MaCV varchar(5) primary key not null,
+	TenCV nvarchar(50)
+	)
+
+create table NHANVIEN(
+	MaNV varchar(5) primary key not null,
+	TenNV nvarchar(50),
+	GioiTinh nvarchar(3) check(GioiTinh = N'Nam' or GioiTinh =N'Nữ'),
+	SDT char(11) check ( SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' or SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') not null,
+	MaCV varchar(5) constraint FK_NV_CV foreign key references CHUCVU(MaCV) not null,
+)
+
+create table PHUONGTIEN(
+	MaPT varchar(5) primary key not null,
+	TenPT nvarchar(50),
+	BienSo nvarchar(20) unique,
+	MaNV varchar(5) constraint FK_PT_NV foreign key references NHANVIEN(MaNV) not null,
+)
+
+create table KHACHSAN(
+	MaKS varchar(5) primary key not null,
+	TenKS nvarchar(50),
+	LoaiKS char(1) check ( LoaiKS like '[1-5]') not null ,
+	DiaChi nvarchar(50),
+	SDT char(11) check ( SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' or SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') not null
+)
+
+create table DIADIEMTHAMQUAN(
+	MaDD varchar(5) primary key not null,
+	TenDD nvarchar(50) ,
+	ThanhPho nvarchar(50),
+	MaKS varchar(5) constraint FK_DDTQ_KS foreign key references KHACHSAN(MaKS) not null,
+)
+
+create table TOUR(
+	MaTour varchar(5) primary key not null,
+	TenTour nvarchar(50) ,
+	SoNgay int,
+	SoDem int,
+	MaPT varchar(5) constraint FK_TOUR_PT foreign key references PHUONGTIEN(MaPT) not null,
+)
+
+create table CHITIETTHAMQUAN(
+	MaDD varchar(5) constraint FK_CTTQ_DDTQ foreign key references DIADIEMTHAMQUAN(MaDD) not null,
+	MaTour varchar(5) constraint FK_CTTQ_TOUR foreign key references TOUR(MaTour) not null,
+	constraint PK_MaDD_MaTour primary key (MaDD,MaTour)
+)
+
+create table KHACHHANG(
+	MaKH varchar(5) primary key not null,	
+	TenKH nvarchar(50) ,
+	DiaChi nvarchar(50),
+	SDT char(11) check ( SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' or SDT like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') not null
+)
+
+create table DOAN(
+	MaDoan varchar(5) primary key not null,
+	TenDoan nvarchar(50) ,
+	MaTour varchar(5) constraint FK_DOAN_TOUR foreign key references TOUR(MaTour) not null,
+	MaNV varchar(5) constraint FK_DOAN_NV foreign key references NHANVIEN(MaNV) not null,
+	MaNDD varchar(5) constraint FK_DOAN_KH foreign key references KHACHHANG(MaKH) not null
+)
+
+create table HOPDONG(
+	MaKH varchar(5) constraint FK_HD_KH foreign key references KHACHHANG(MaKH) not null,
+	MaDoan varchar(5) constraint FK_HD_DOAN foreign key references DOAN(MaDoan) not null,
+	NgayLapHD smalldatetime not null,
+	NgayDi smalldatetime not null,
+	NoiDon nvarchar(50),
+	constraint PK_MaKH_MaDoan primary key (MaKH,MaDoan),
+	constraint check_HD check (Ngaydi > NgayLapHD)
+)
+
+create table CHUYEN(
+	MaChuyen varchar(5) primary key not null,
+	TenChuyen nvarchar(50) ,
+	MaTour varchar(5) constraint FK_CHUYEN_TOUR foreign key references TOUR(MaTour) not null,
+	MaNV varchar(5) constraint FK_CHUYEN_NV foreign key references NHANVIEN(MaNV) not null
+)
+
+create table DANGKY(
+	MaKH varchar(5) constraint FK_DK_KH foreign key references KHACHHANG(MaKH) not null,
+	MaChuyen varchar(5) constraint FK_DK_CHUYEN foreign key references Chuyen(MaChuyen) not null,
+	NgayDi smalldatetime not null,
+	NoiKhoiHanh nvarchar(50),
+	constraint PK_MaKH_MaChuyen primary key (MaKH,MaChuyen),
+)
+
---
+++ BaoCao/query.sql
@@ -0,0 +1,85 @@
+﻿---*** 1.Xuất ra thông tin nhân viên có giới tính nữ
+SELECT * FROM NHANVIEN WHERE GioiTinh=N'Nữ'
+
+---*** 2.Xuất ra danh sách nhân viên có số điện thoại là 11 chữ số và có  chức vụ là hướng dẫn viên
+SELECT * FROM NHANVIEN NV,CHUCVU CV WHERE NV.MaCV=CV.MaCV AND LEN(SDT) =11 AND TenCV = N'Hướng dẫn viên' 
+
+---*** 3.Xuất ra danh sách  các tour do nhân viên Doãn Chí Bình làm tài xế
+SELECT TenTour FROM TOUR T,NHANVIEN NV ,PHUONGTIEN PT WHERE PT.MaPT=T.MaPT AND PT.MaNV=NV.MaNV AND NV.TenNV=N'Doãn Chí Bình'  
+
+---*** 4.Tìm nhân viên hướng dẫn nhiều đoàn nhất
+SELECT *FROM NHANVIEN NV INNER JOIN
+(SELECT TOP 1 MaNV,Count(*) AS TONG FROM DOAN GROUP BY MaNV ORDER BY TONG DESC) S
+ON NV.MANV = S.MaNV
+
+---*** 5.Tìm nhân viên hướng dẫn chuyến nhiều nhất
+SELECT *FROM NHANVIEN NV INNER JOIN
+(SELECT TOP 1 MaNV,Count(*) AS TONG FROM CHUYEN GROUP BY MaNV ORDER BY TONG DESC) S
+ON NV.MANV = S.MaNV;
+
+---*** 6.Tìm nhân viên có họ Nguyễn
+SELECT * FROM NHANVIEN WHERE TenNV LIKE N'Nguyễn%'
+
+---***7.Tìm những hợp đồng được ký kết vào năm 2019
+SELECT * FROM HOPDONG WHERE YEAR(NgayLapHD)=2019
+
+---***8.Xuất ra địa điểm tham quan có khách sạn 4 sao trở lên
+SELECT TenDD FROM DIADIEMTHAMQUAN WHERE MaKS IN (
+SELECT MaKS FROM KHACHSAN WHERE LoaiKS='4' OR  LoaiKS='5' )
+
+---***9.Xuất ra thông tin khách hàng tham gia đoàn du lịch trong khoảng tháng 3/2018 đến 5/2018
+SELECT *FROM KHACHHANG WHERE MaKH IN (
+SELECT MaKh FROM HOPDONG WHERE NgayDi BETWEEN '2018/03/01' AND '2018/05/30'
+)
+---***10.Cho biết  đoàn có số lượng khách lớn nhất của hợp đồng
+SELECT TOP 1 D.MaDoan,Tong FROM DOAN D INNER JOIN(
+SELECT D.MaDoan ,COUNT(*) AS 'Tong'
+FROM DOAN D,HOPDONG HD 
+WHERE D.MaDoan= HD.MaDoan
+GROUP BY D.MaDoan
+) AS T
+ON T.MaDoan=D.MaDoan
+
+
+---***11.Cho biết  chuyến có số lượng khách lớn nhất khi đăng ký
+
+SELECT TOP 1 C.MaChuyen,Tong FROM CHUYEN C INNER JOIN(
+SELECT C.MaChuyen ,COUNT(*) AS 'Tong'
+FROM CHUYEN C,DANGKY DK 
+WHERE C.MaChuyen= DK.MaChuyen
+GROUP BY C.MaChuyen
+) AS T
+ON T.MaChuyen=C.MaChuyen
+
+---***12.Danh sách các đoàn có địa điểm tham quan tại thành phố HCM
+SELECT * FROM DOAN WHERE MaTour IN(
+SELECT MaTour FROM CHITIETTHAMQUAN WHERE MADD IN (
+SELECT MADD FROM DIADIEMTHAMQUAN WHERE ThanhPho=N'HCM')
+)
+
+
+
+
+
+(SELECT MaNV,Count(*) FROM CHUYEN GROUP BY MaNV) 
+SELECT MaNV,Count(*) FROM DOAN GROUP BY MaNV)
+
+
+
+SELECT * FROM CHITIETTHAMQUAN
+
+SELECT * FROM DIADIEMTHAMQUAN
+
+SELECT DISTINCT C.MaNV,D.MaNV FROM CHUYEN C,DOAN D
+
+SELECT NV.MaNV FROM NHANVIEN NV,DOAN D,CHUYEN C WHERE NV.MaNV=D.MaNV OR NV.MaNV=C.MaNV 
+
+SELECT MaNV,Count(*) FROM CHUYEN GROUP BY MaNV 
+SELECT MaNV,Count(*) FROM DOAN GROUP BY MaNV
+
+SELECT MaNV,Count(*) FROM CHUYEN GROUP BY MaNV
+UNION(
+SELECT MaNV,Count(*) FROM DOAN GROUP BY MaNV
+)
+
+SELECT * FROM NHANVIEN
